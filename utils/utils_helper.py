882f9ace8d145878ca21f3df9b529e6ee40c45aa
talha309 <tiseoexpert@gmail.com>
2025-03-16 23:33:13 +0500

    create token

--- utils/utils_helper.py
+++ utils/utils_helper.py
@@ -1,11 +1,34 @@
-import os 
+import os
 import jwt
+from typing import Optional
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 
-
+# Load environment variables
 load_dotenv()
 
-
-SECRET_KEY =  os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
-ACCESS_TOKEN_EXPIRE_MINUTES =  os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 15))  # Convert to integer
+
+# Create token
+def create_access_token(data: dict, expire_delta: Optional[timedelta] = None):
+    try:
+        to_encode = data.copy()
+        expire = datetime.utcnow() + (expire_delta or timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
+        to_encode.update({"exp": expire})
+        return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    except Exception as e:
+        print('An exception occurred')
+        print(e)
+        return None
+
+# Decode the token
+def decode_access_token(token: str):
+    try:
+        decode_token = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return decode_token
+    except Exception as e:
+        print('An exception occurred')
+        print(e)
+        return None
