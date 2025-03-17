7719bc5c15d1974f24f12b6de60d02bc6be52730
talha309 <tiseoexpert@gmail.com>
2025-03-17 21:49:38 +0500

    apply constrain using pydantic laberies

--- validations/validation.py
+++ validations/validation.py
@@ -1,9 +1,9 @@
-from pydantic import BaseModel, Field, validator, root_validator
-from typing import Optional
+from pydantic import BaseModel, Field, validator
+from typing import Optional, List, Any
 
 class StandardResponse(BaseModel):
     message: str = ""
-    data: Optional[list] = None
+    data: Optional[List[Any]] = None
     status: str = ""
 
 class TodoCreate(BaseModel):
@@ -20,16 +20,8 @@ class UserCreate(BaseModel):
     email: str = Field(..., regex=r'^\S+@\S+$')
     password: str = Field(..., min_length=6)
 
-    @validator('name')
-    def validate_name_length(cls, v):
-        if len(v) < 3 or len(v) > 50:
-            raise ValueError('Name must be between 3 and 50 characters')
-        return v
-
-    # For the number validator, you can add an example validator like this:
     @validator('password')
     def validate_password_length(cls, v):
         if len(v) < 6:
             raise ValueError('Password must be at least 6 characters')
         return v
-
